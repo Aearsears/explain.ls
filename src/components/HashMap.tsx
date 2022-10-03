8d1615a03213685beea1422727b4c00df730c31a
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-10-02 23:36:36 -0400

    add question and hashmap components

---
+++ src/components/HashMap.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HashMap() {
+    const map = new Map<number, number>();
+    map.set(1, 1);
+    return (
+        <div>
+            <h1>HashMap</h1>
+        </div>
+    );
+}
+
+export default HashMap;
